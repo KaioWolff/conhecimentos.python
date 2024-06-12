db53aa1326b6c8bb19d04b30a4a51a2cfbd439a2
KaioWolff <kaiolorentzwolff@gmail.com>
2024-06-12 16:57:38 -0300

    aula2.py
    teste velocidade

--- aula2.py
+++ aula2.py
@@ -1,1 +1,21 @@
-print("Olá")
+def controlevelocidade():
+    velocidade = 50
+    contator = 0
+
+while contatortentativas < 10 and 0 <= velocidade <= 100:
+    print(f"velocidade atual: {velocidade} km")
+    input=("deseja aumentar (A) ou diminuir (D) a velocidade?")
+    if opcao == "A":
+        velocidade += 10
+    elif opcao == "D":
+        velocidade -= 10
+
+    else:
+        print("escolha invalida,tente novamente.")
+
+    contator += 1
+
+    if velocidade <= 0 or velocidade >= 100:
+        break
+     print(f "operacao finalizada,velocidade final: {velocidade} km")
+

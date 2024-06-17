58668b4b87edc9dc040ad5f5b0e54ba28ffe24b4
KaioWolff <kaiolorentzwolff@gmail.com>
2024-06-17 14:15:23 -0300

    programa de contator de velocidade
    controle de velocidade.

--- aula2.py
+++ aula2.py
@@ -1,21 +1,23 @@
-def controlevelocidade():
+def controledevelocidade():
     velocidade = 50
     contator = 0
+    print("Chamou")
+    while contator < 10 and velocidade >= 0 and velocidade <= 100:
+        print(f"velocidade atual= {velocidade} km")
+        opcao = input("deseja aumentar (A) ou diminuir (D) a velocidade?").strip().upper()
+        if opcao == "A":
+            velocidade += 10
+        elif opcao == "D":
+            velocidade -= 10
+        else:
+            print("escolha invalida,tente novamente.")
+            continue
 
-while contatortentativas < 10 and 0 <= velocidade <= 100:
-    print(f"velocidade atual: {velocidade} km")
-    input=("deseja aumentar (A) ou diminuir (D) a velocidade?")
-    if opcao == "A":
-        velocidade += 10
-    elif opcao == "D":
-        velocidade -= 10
+        contator += 1
 
-    else:
-        print("escolha invalida,tente novamente.")
+        if velocidade <= 0 or velocidade >= 100:
+            break
+        print(f"operacao finalizada,velocidade final: {velocidade} km")
 
-    contator += 1
-
-    if velocidade <= 0 or velocidade >= 100:
-        break
-     print(f "operacao finalizada,velocidade final: {velocidade} km")
+controledevelocidade()
 
